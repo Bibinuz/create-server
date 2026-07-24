d19e669f4e4c9a0590afc8d9a55d4420658c2e9e
Biel Alavedra <biel.alavedra@gmail.com>
2026-07-24 14:13:34 +0200

    Everything is fine

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-java -XX:+UseZGC -XX:+ZGenerational @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.233/unix_args.txt "$@"
+/home/bibinuz/.local/share/PrismLauncher/java/java-runtime-delta/bin/java -XX:+UseZGC -XX:+ZGenerational @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.233/unix_args.txt "$@"
 
