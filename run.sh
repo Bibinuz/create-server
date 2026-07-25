7d36b6f0bf5be68ea0d35d157a338340cd843e1e
Biel Alavedra <biel.alavedra@gmail.com>
2026-07-25 10:26:54 +0200

    backup 2

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env sh
-/home/bibinuz/.local/share/PrismLauncher/java/java-runtime-delta/bin/java -XX:+UseZGC -XX:+ZGenerational @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.233/unix_args.txt "$@"
-
+/home/bibinuz/.local/share/PrismLauncher/java/java-runtime-delta/bin/java -XX:+UseZGC -XX:+ZGenerational @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.233/unix_args.txt nogui "$@"
